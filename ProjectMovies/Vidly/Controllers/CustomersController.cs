4761a415d5305aa13ac3491944b42e7810c96d1d
Diego Vidal <diegovidal@MacBook-Pro-de-Diego.local>
2016-06-09 15:45:15 -0300

    agora vai

---
+++ ProjectMovies/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult ListCustomers()
+        {
+
+            var customers = new List<Customer>
+            {
+                new Customer{ Name = "Diego Vidal", Id = 0 },
+                new Customer{ Name = "Victor Vidal", Id = 1 },
+                new Customer{ Name = "Guilherme Augusto", Id = 2 },
+                new Customer{ Name = "Waldir de Paula", Id = 3 }
+
+            };
+
+            var viewModel = new ListCustomersViewModel
+            {
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        [Route("customers/customerdetails/{customer}")]
+        public ActionResult CustomerDetails(string customer)
+        {
+
+            ViewBag.Customer = customer;
+            //return Content("Olá meu nome é " + customer);
+            return View();
+        }
+    }
+}
---
+++ ProjectMovies/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/ListMovies
+        public ActionResult ListMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Name = "Gladiador", Id = 0},
+                new Movie { Name = "Taken", Id = 1 },
+                new Movie { Name = "Harry Potter", Id = 2 },
+            };
+
+            var viewModel = new ListMoviesViewModel 
+            {
+                Movies = movies
+            };
+
+
+           // return Content("Conteudo é: " + viewModel.Movies[1].Id);
+            return View(viewModel);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+       
+        }
+
+        [Route("movies/moviedetails/{movie}")]
+        public ActionResult MovieDetails(string movie)
+        {
+            ViewBag.Movie = movie;
+
+            return View();
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = "+ id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        // regex não funcionou
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + " / " + month);
+        }
+    }
+}
